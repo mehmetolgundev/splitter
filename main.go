efb7c44574d0937cf2f5c8615a61417ec41d141b
mehmetolgundev <mehmetolgun42konya@gmail.com>
2024-01-10 22:34:04 +0300

    prometheus

--- main.go
+++ main.go
@@ -4,9 +4,24 @@ import (
 	"log"
 
 	"github.com/mehmetolgundev/splitter/pkg/listener"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	memoryUsage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "splitter_memory_usage",
+			Help: "Current memory usage of the application",
+		},
+		[]string{"instance"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(memoryUsage)
+}
 func main() {
+
 	err := listener.NewTCPListener().Listen()
 	if err != nil {
 		log.Fatal(err)
